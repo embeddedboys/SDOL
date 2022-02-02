7b3dffca7d928128ebb56e114d265b437cfb8cf4
Zheng Hua <writeforever@foxmail.com>
2022-02-02 11:36:31 +0800

    update tests.
    Signed-off-by: Zheng Hua <writeforever@foxmail.com>

--- tests/test_oled.c
+++ tests/test_oled.c
@@ -1,19 +1,40 @@
-/**
- * Copyright (C) 2021 jensenhua(writeforever@foxmail.com)
- * 
- * This file is part of sdol.
- * 
- * sdol is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- * 
- * sdol is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with sdol.  If not, see <http://www.gnu.org/licenses/>.
- */
+#include "system.h"
+#include "oled.h"
 
+static struct oled_operations m_oled_opr;
+
+static struct system_operations sys_opr;
+
+
+
+void SystemInit()
+{
+    P0M0 = 0x00;
+    P0M1 = 0x00;
+    P1M0 = 0x00;
+    P1M1 = 0x00;
+    P2M0 = 0x00;
+    P2M1 = 0x00;
+    P3M0 = 0x00;
+    P3M1 = 0x00;
+    P4M0 = 0x00;
+    P4M1 = 0x00;
+    P5M0 = 0x00;
+    P5M1 = 0x00;
+    
+    register_system_operations( &sys_opr );
+    sys_opr.iomux( P32P33_I2C );
+    
+    register_oled_operations( &m_oled_opr );
+    m_oled_opr.init();
+}
+
+void main()
+{
+    SystemInit();
+    
+    m_oled_opr.putascii( 5, 5, 'A' );
+    m_oled_opr.flush();
+    
+    while( 1 );
+}
